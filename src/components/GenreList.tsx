8ce48757f001bbb55f9649f05507a06f55e159fe
Ahmad Yunus <yunusmujadidi@gmail.com>
2023-11-16 16:07:28 +0700

    Add loading and error handling to GenreList component

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,8 +1,10 @@
-import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
 const GenreList = () => {
-  const { data } = useGenres();
+  const { data, isLoading, error } = useGenres();
+  if (error) return <Text>Failed to load genres</Text>;
+  if (isLoading) return <Spinner />;
   return (
     <List>
       {data.map((genre) => (
